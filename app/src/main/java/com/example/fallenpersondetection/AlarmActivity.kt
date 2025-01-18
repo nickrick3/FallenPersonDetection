13a1151bc4b488e8d805d76a1000728bb0a1950f
nickrick3 <nik3.riccardi@gmail.com>
2025-01-18 20:13:05 +0100

    simple alarm activity

--- app/src/main/java/com/example/fallenpersondetection/AlarmActivity.kt
+++ app/src/main/java/com/example/fallenpersondetection/AlarmActivity.kt
@@ -1,11 +1,39 @@
 package com.example.fallenpersondetection
 
+import android.media.AudioAttributes
+import android.media.RingtoneManager
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 
 class AlarmActivity: ComponentActivity() {
+    private val mRingtoneManager = RingtoneManager(this)
+    private val alarm = mRingtoneManager.getRingtone(R.raw.alarm)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fall_alarm)
+        alarm.audioAttributes = AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_ALARM)
+            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+            .build()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        alarm.play()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (alarm.isPlaying) {
+            alarm.stop()
+        }
+    }
+
+    fun stopAlarm(v: View) {
+        if (alarm.isPlaying) {
+            alarm.stop()
+        }
     }
 }
