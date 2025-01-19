cfb9087e98b592a459666da278aa4b1cea2db57a
nickrick3 <nik3.riccardi@gmail.com>
2025-01-20 00:41:58 +0100

    ask for permission to start activities

--- app/src/main/java/com/example/fallenpersondetection/MainActivity.kt
+++ app/src/main/java/com/example/fallenpersondetection/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.fallenpersondetection
 
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.DialogInterface.OnClickListener
 import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
+import android.view.View
 import android.widget.CompoundButton
 import android.widget.CompoundButton.OnCheckedChangeListener
 import android.widget.Switch
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 
-class MainActivity : ComponentActivity(), OnCheckedChangeListener {
+class MainActivity : ComponentActivity(), OnCheckedChangeListener, OnClickListener{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,13 +25,46 @@ class MainActivity : ComponentActivity(), OnCheckedChangeListener {
         detectionSwitch.setOnCheckedChangeListener(this)
     }
 
+    override fun onResume() {
+        super.onResume()
+        if (Settings.canDrawOverlays(this)) {
+            val tw : TextView = findViewById(R.id.permissionGranted)
+            tw.visibility = View.VISIBLE
+        }
+    }
+
     override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
         val intent = Intent(this, Accelerometer::class.java)
         if (isChecked) {
-            startService(intent)
+            // check if user has already given permission
+            if (Settings.canDrawOverlays(this)) {
+                // start service
+                startService(intent)
+            }
+            else {
+                // uncheck button
+                buttonView?.isChecked = false
+
+                // ask for permission
+                AlertDialog.Builder(this)
+                    .setMessage("This feature requires over lay permission")
+                    .setPositiveButton("OK", this)
+                    .setNegativeButton("Cancel", this)
+                    .create()
+                    .show()
+            }
         }
         else {
+            // stop service
             stopService(intent)
         }
     }
+
+    override fun onClick(dialog: DialogInterface?, which: Int) {
+        if (which == DialogInterface.BUTTON_POSITIVE) {
+            // go to settings
+            startActivity(Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION))
+        }
+    }
+
 }
